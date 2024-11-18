d57cee10a39e665b87d24fc6cc3c4cb214b6d1ef
mensajera <mensajera798@gmail.com>
2024-11-18 12:39:14 -0400

    hola

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { PORT } from "./routes/config.js";
 async function main (){
     try {
         await sequelize.sync({
-            force:true
+            force:false
         });  /* esta es una manera de probar la coneccion a la base de datos */
         // hola
       
